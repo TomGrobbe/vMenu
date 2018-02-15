89a5bdad3a4a206d131bbceb138721d189af0882
Tom Grobbe <31419184+TomGrobbe@users.noreply.github.com>
2018-02-15 21:54:10 +0100

    Old

--- vMenu/Properties/AssemblyInfo.cs
+++ vMenu/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("vMenuServer")]
+[assembly: AssemblyTitle("vMenu")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("vMenuServer")]
+[assembly: AssemblyProduct("vMenu")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9e3b0f3e-2203-4bdf-b695-ded55d4edd23")]
+[assembly: Guid("58fe5d89-e729-4246-b527-6eda2a1af04d")]
 
 // Version information for an assembly consists of the following four values:
 //
