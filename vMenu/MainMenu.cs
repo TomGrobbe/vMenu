b6b2e58eab157d3ae6ec1c4653a6b652b497bb45
Tom Grobbe <31419184+TomGrobbe@users.noreply.github.com>
2018-02-15 22:35:37 +0100

    Added all required references, configured project settings

---
+++ vMenu/MainMenu.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+
+namespace vMenu
+{
+    public class MainMenu : BaseScript
+    {
+        private bool firstTick = true;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
+        public MainMenu()
+        {
+            Tick += OnTick;
+        }
+
+        /// <summary>
+        /// OnTick runs every game tick.
+        /// </summary>
+        /// <returns></returns>
+        private async Task OnTick()
+        {
+            if (firstTick)
+            {
+                firstTick = false;
+                await Delay(0);
+            }
+        }
+
+
+    }
+}
