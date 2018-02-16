5c9bceaaa88d5edeb752aeec79c6bd5329a742ea
Tom Grobbe <31419184+TomGrobbe@users.noreply.github.com>
2018-02-16 15:35:42 +0100

    More player options and ped scenarios added

--- vMenu/menus/PlayerOptionsMenu.cs
+++ vMenu/menus/PlayerOptionsMenu.cs
@@ -25,7 +25,7 @@ namespace vMenu.menus
         private bool neverWanted = false;
         private bool everyoneIgnoresPlayer = false;
         private bool playerFrozen = false;
-        private enum Scenarios {NONE, LEANING, SMOKING, SUNBATH, SUNBATH2};
+        private enum Scenarios { NONE, LEANING, SMOKING, SUNBATH, SUNBATH2 };
         private bool driving = false;
 
         public PlayerOptionsMenu()
@@ -61,8 +61,6 @@ namespace vMenu.menus
                 SetPlayerWantedLevelNow(PlayerId(), false);
             }
             SetEveryoneIgnorePlayer(PlayerId(), everyoneIgnoresPlayer);
-            
-
         }
 
         private void CreateMenu()
@@ -79,20 +77,96 @@ namespace vMenu.menus
             UIMenuCheckboxItem everyoneIgnoresPlayerCheckbox = new UIMenuCheckboxItem("Everyone Ignores You", everyoneIgnoresPlayer, "Annoying hillbillies trying to kill you? Turn this off and the Snail will make them shit their pants!");
             UIMenuCheckboxItem playerFrozenCheckbox = new UIMenuCheckboxItem("Freeze Yourself", playerFrozen, "Why would you do this...?");
 
-            // Other options
-            List<dynamic> wantedLevelList = new List<dynamic> { 0, 1, 2, 3, 4};
+            // Wanted level options
+            List<dynamic> wantedLevelList = new List<dynamic> { 0, 1, 2, 3, 4 };
             UIMenuListItem setWantedLevel = new UIMenuListItem("Set Wanted Level", wantedLevelList, GetPlayerWantedLevel(PlayerId()), "Set the wanted level by selecting a value, and pressing enter.");
+            // Player options
+            List<dynamic> playerOptionsList = new List<dynamic> { "Heal Player", "Apply Max Armor", "Clean Player", "Dry Player", "Soack Player" };
+            UIMenuListItem playerOptions = new UIMenuListItem("Player Options", playerOptionsList, 0, "Select an option and press enter to execute it.");
+            // Actions
+            List<dynamic> playerActionsList = new List<dynamic> { "Commit Suicide", "Drive To Waypoint", "Drive Wander" };
+            UIMenuListItem playerActions = new UIMenuListItem("Player Actions", playerActionsList, 0, "Select an action and press enter to run it, use the cancel button below to stop the driving actions.");
+            UIMenuItem cancelActions = new UIMenuItem("Cancel Player Actions", "Click this to cancel any of the driving player actions from the list above.");
+            /// Scenarios (list is in <cref see="PedScenarios.cs">PedScenarios.cs</cref>
+            UIMenuListItem playerScenarios = new UIMenuListItem("Scenarios", PedScenarios.Scenarios, 0, "Select a scenario and hit enter to start it. Press it again to cancel it. Selecting another scenario and hitting enter will override the current scenario. Pressing enter again will then stop the scenario.");
 
+            // Add all items to the menu.
+            menu.AddItem(playerGodModeCheckbox);
+            menu.AddItem(invisibleCheckbox);
+            menu.AddItem(unlimitedStaminaCheckbox);
+            menu.AddItem(fastRunCheckbox);
+            menu.AddItem(fastSwimCheckbox);
+            menu.AddItem(superJumpCheckbox);
+            menu.AddItem(noRagdollCheckbox);
+            menu.AddItem(neverWantedCheckbox);
+            menu.AddItem(setWantedLevel);
+            menu.AddItem(everyoneIgnoresPlayerCheckbox);
+            menu.AddItem(playerOptions);
+            menu.AddItem(playerActions);
+            menu.AddItem(playerFrozenCheckbox);
+            menu.AddItem(playerScenarios);
 
+            // Handle all checkbox changes.
+            menu.OnCheckboxChange += (sender, item, _checked) =>
+            {
+                // Toggle godmode.
+                if (item == playerGodModeCheckbox)
+                {
+                    godMode = _checked;
+                }
+                // Toggle invisibility.
+                if (item == invisibleCheckbox)
+                {
+                    invisible = _checked;
+                }
+                if (item == unlimitedStaminaCheckbox)
+                {
+                    unlimitedStamina = _checked;
+                }
+                if (item == fastRunCheckbox)
+                {
+                    if (_checked)
+                    {
+                        SetRunSprintMultiplierForPlayer(PlayerId(), 1.49f);
+                    }
+                    else
+                    {
+                        SetRunSprintMultiplierForPlayer(PlayerId(), 1f);
+                    }
+                }
+                if (item == fastSwimCheckbox)
+                {
+                    if (_checked)
+                    {
+                        SetSwimMultiplierForPlayer(PlayerId(), 1.49f);
+                    }
+                    else
+                    {
+                        SetSwimMultiplierForPlayer(PlayerId(), 1f);
+                    }
+                }
+                if (item == superJumpCheckbox)
+                {
+                    superJump = _checked;
+                }
+                if (item == noRagdollCheckbox)
+                {
+                    noRagdoll = _checked;
+                }
+                if (item == neverWantedCheckbox)
+                {
+                    neverWanted = _checked;
+                }
+                if (item == everyoneIgnoresPlayerCheckbox)
+                {
+                    everyoneIgnoresPlayer = _checked;
+                }
+                if (item == playerFrozenCheckbox)
+                {
+                    FreezeEntityPosition(PlayerPedId(), playerFrozen);
+                }
+            };
 
-            //if (fastRun)
-            //{
-            //    SetRunSprintMultiplierForPlayer(PlayerId(), 1.49f);
-            //}
-            //if (fastSwim)
-            //{
-            //    SetSwimMultiplierForPlayer(PlayerId(), 1.49f);
-            //}
 
             //FreezeEntityPosition(PlayerPedId(), playerFrozen);
 
