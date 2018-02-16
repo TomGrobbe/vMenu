3d4c531bb1bc0a09dbc56e41d3319d7e129091af
Tom Grobbe <31419184+TomGrobbe@users.noreply.github.com>
2018-02-16 17:30:36 +0100

    Finished player options up.
    Only need to add the driving task features, will do that some other time. Also still need to setup the menu itself.

--- vMenu/Notification.cs
+++ vMenu/Notification.cs
@@ -8,17 +8,13 @@ using static CitizenFX.Core.Native.API;
 
 namespace vMenu
 {
-    #region Notifications Class
+    #region Notifications Struct
     /// <summary>
-    /// Notifications class to easilly show notifications using custom made templates,
+    /// Notifications struct to easilly show notifications using custom made templates,
     /// or completely custom style if none of the templates are fitting for the current task.
     /// </summary>
-    public class Notification : BaseScript
+    public struct Notification
     {
-        public Notification()
-        {
-        }
-
         /// <summary>
         /// Show a custom notification above the minimap.
         /// </summary>
@@ -75,6 +71,78 @@ namespace vMenu
         {
             Custom("~g~~h~Success~w~: " + message, blink, saveToBrief);
         }
+    }
+    #endregion
+
+    #region Custom Subtitles Struct
+    /// <summary>
+    /// Custom Subtitles struct used to display subtitles using preformatted templates.
+    /// Optionally you can also use a blank/custom style if you don't want to use an existing template.
+    /// </summary>
+    public struct Subtitles
+    {
+
+        /// <summary>
+        /// Custom (white/custom text style subtitle)
+        /// </summary>
+        /// <param name="message">The message to be displayed.</param>
+        /// <param name="duration">(Optional) duration in ms.</param>
+        /// <param name="drawImmediately">(Optional) draw the notification immediately or wait for the previous subtitle text to disappear.</param>
+        public void Custom(string message, int duration = 2500, bool drawImmediately = true)
+        {
+            BeginTextCommandPrint("THREESTRINGS");
+            AddTextComponentSubstringPlayerName(message);
+            EndTextCommandPrint(duration, drawImmediately);
+        }
+
+        /// <summary>
+        /// Alert (yellow text subtitle).
+        /// </summary>
+        /// <param name="message">The message to be displayed.</param>
+        /// <param name="duration">(Optional) duration in ms.</param>
+        /// <param name="drawImmediately">(Optional) draw the notification immediately or wait for the previous subtitle text to disappear.</param>
+        /// <param name="prefix">(Optional) add a prefix to your message, if you use this, only the prefix will be colored. The rest of the message will be left white.</param>
+        public void Alert(string message, int duration = 2500, bool drawImmediately = true, string prefix = null)
+        {
+            Custom((prefix != null ? "~y~" + prefix + " ~w~" : "~y~") + message, duration, drawImmediately);
+        }
+
+        /// <summary>
+        /// Error (red text subtitle).
+        /// </summary>
+        /// <param name="message">The message to be displayed.</param>
+        /// <param name="duration">(Optional) duration in ms.</param>
+        /// <param name="drawImmediately">(Optional) draw the notification immediately or wait for the previous subtitle text to disappear.</param>
+        /// <param name="prefix">(Optional) add a prefix to your message, if you use this, only the prefix will be colored. The rest of the message will be left white.</param>
+        public void Error(string message, int duration = 2500, bool drawImmediately = true, string prefix = null)
+        {
+            Custom((prefix != null ? "~r~" + prefix + " ~w~" : "~r~") + message, duration, drawImmediately);
+        }
+
+        /// <summary>
+        /// Info (blue text subtitle).
+        /// </summary>
+        /// <param name="message">The message to be displayed.</param>
+        /// <param name="duration">(Optional) duration in ms.</param>
+        /// <param name="drawImmediately">(Optional) draw the notification immediately or wait for the previous subtitle text to disappear.</param>
+        /// <param name="prefix">(Optional) add a prefix to your message, if you use this, only the prefix will be colored. The rest of the message will be left white.</param>
+        public void Info(string message, int duration = 2500, bool drawImmediately = true, string prefix = null)
+        {
+            Custom((prefix != null ? "~b~" + prefix + " ~w~" : "~b~") + message, duration, drawImmediately);
+        }
+
+        /// <summary>
+        /// Success (green text subtitle).
+        /// </summary>
+        /// <param name="message">The message to be displayed.</param>
+        /// <param name="duration">(Optional) duration in ms.</param>
+        /// <param name="drawImmediately">(Optional) draw the notification immediately or wait for the previous subtitle text to disappear.</param>
+        /// <param name="prefix">(Optional) add a prefix to your message, if you use this, only the prefix will be colored. The rest of the message will be left white.</param>
+        public void Success(string message, int duration = 2500, bool drawImmediately = true, string prefix = null)
+        {
+            Custom((prefix != null ? "~g~" + prefix + " ~w~" : "~g~") + message, duration, drawImmediately);
+        }
+
     }
     #endregion
 }
--- vMenu/menus/PlayerOptionsMenu.cs
+++ vMenu/menus/PlayerOptionsMenu.cs
@@ -11,23 +11,27 @@ namespace vMenu.menus
 {
     class PlayerOptionsMenu : BaseScript
     {
+        // Variables
         private UIMenu menu;
         private CommonFunctions cf = new CommonFunctions();
         private Notification Notify = new Notification();
+        private Subtitles Subtitle = new Subtitles();
 
         private bool godMode = false;
         private bool invisible = false;
         private bool unlimitedStamina = true;
-        private bool fastRun = false;
-        private bool fastSwim = false;
         private bool superJump = false;
         private bool noRagdoll = false;
         private bool neverWanted = false;
         private bool everyoneIgnoresPlayer = false;
         private bool playerFrozen = false;
-        private enum Scenarios { NONE, LEANING, SMOKING, SUNBATH, SUNBATH2 };
         private bool driving = false;
 
+        private int IsPlayingScenario = -1;
+
+        /// <summary>
+        /// Constructor.
+        /// </summary>
         public PlayerOptionsMenu()
         {
             menu = new UIMenu("Player Options", "Common player options.")
@@ -39,38 +43,104 @@ namespace vMenu.menus
             CreateMenu();
 
             Tick += OnTick;
+            Tick += ScenarioTick;
         }
 
+        /// <summary>
+        /// OnTick used for all basic options that need to be set every tick.
+        /// </summary>
+        /// <returns></returns>
         private async Task OnTick()
         {
+            // God mode
             SetEntityInvincible(PlayerPedId(), godMode);
+
+            // Invisibility
             SetEntityVisible(PlayerPedId(), !invisible, false);
+            
+            // Unlimited Stamina
             if (unlimitedStamina)
             {
                 ResetPlayerStamina(PlayerId());
             }
+            
+            // Super Jump
             if (superJump)
             {
                 SetSuperJumpThisFrame(PlayerId());
             }
+            
+            // No Ragdoll
             SetPedCanRagdoll(PlayerPedId(), !noRagdoll);
             SetPedCanRagdollFromPlayerImpact(PlayerPedId(), !noRagdoll);
+            
+            // Never Wanted
             if (neverWanted && GetPlayerWantedLevel(PlayerId()) > 0)
             {
-                SetPlayerWantedLevel(PlayerId(), 0, false);
-                SetPlayerWantedLevelNow(PlayerId(), false);
+                ClearPlayerWantedLevel(PlayerId());
             }
+            
+            // Everyone Ignores Player
             SetEveryoneIgnorePlayer(PlayerId(), everyoneIgnoresPlayer);
+        }
+        
+        /// <summary>
+        /// OnTick used for playing/stopping scenarios.
+        /// </summary>
+        /// <returns></returns>
+        private async Task ScenarioTick()
+        {
+            // Each game tick, check if the player is using a scenario.
+            if (IsPlayingScenario != -1)
+            {
+                // If so, set the current scenario to the playing scenario.
+                var currentScenario = IsPlayingScenario;
+                // Then loop below and wait until the player is no longer playing a scenario (IsPlayingScenario == -1).
+                while (IsPlayingScenario != -1)
+                {
+                    // Wait to prevent crashing.
+                    await Delay(0);
+                    // If the scenario is not the "current" scenario and it's not -1 ( = no scenario) then:
+                    if (IsPlayingScenario != currentScenario && IsPlayingScenario != -1)
+                    {
+                        if (CanPlayScenarios())
+                        {
+                            // Clear the tasks (any scenarios).
+                            ClearPedTasks(PlayerPedId());
+                            // Start the new scenario.
+                            TaskStartScenarioInPlace(PlayerPedId(), PedScenarios.ScenarioNames[PedScenarios.Scenarios[IsPlayingScenario]], 0, true);
+                            // Set the current scenario to be the new scenario.
+                            currentScenario = IsPlayingScenario;
+                        }
+                        else
+                        {
+                            Notify.Alert("You can only start a scenario if you're standing still, and you're not inside any vehicle.");
+                            ClearPedTasksImmediately(PlayerPedId());
+                            IsPlayingScenario = -1;
+                            currentScenario = IsPlayingScenario;
+                        }
+                        
+                    }
+                    // Keep looping until the scenario id = -1s
+                }
+                // When it's -1, clear the player tasks once.
+                if (IsPlayingScenario == -1)
+                {
+                    ClearPedTasks(PlayerPedId());
+                }
+                // Then go back to the regular OnTick loop and wait for any new scenarios to be selected.
+            }
         }
 
         private void CreateMenu()
         {
+            #region Create Menu Items
             // Checkboxes
             UIMenuCheckboxItem playerGodModeCheckbox = new UIMenuCheckboxItem("God Mode", godMode, "If you turn this on, you won't take any damage.");
             UIMenuCheckboxItem invisibleCheckbox = new UIMenuCheckboxItem("Invisibility", invisible, "If you turn this on, you will become invisible.");
             UIMenuCheckboxItem unlimitedStaminaCheckbox = new UIMenuCheckboxItem("Unlimited Stamina", unlimitedStamina, "If you disable this then you won't be able to run for more than 5 seconds. So it's recommended to keep this on at all times.");
-            UIMenuCheckboxItem fastRunCheckbox = new UIMenuCheckboxItem("Fast Running", fastRun, "Super Snail! I'm fast as f*ck boi!!!");
-            UIMenuCheckboxItem fastSwimCheckbox = new UIMenuCheckboxItem("Fast Swimming", fastSwim, "Super Sail 2.0! Swim like a real snail!");
+            UIMenuCheckboxItem fastRunCheckbox = new UIMenuCheckboxItem("Fast Running", false, "Super Snail! I'm fast as f*ck boi!!!");
+            UIMenuCheckboxItem fastSwimCheckbox = new UIMenuCheckboxItem("Fast Swimming", false, "Super Sail 2.0! Swim like a real snail!");
             UIMenuCheckboxItem superJumpCheckbox = new UIMenuCheckboxItem("Super Jump", superJump, "Super Snail 3.0! You can't beat a snail's jumping skills!");
             UIMenuCheckboxItem noRagdollCheckbox = new UIMenuCheckboxItem("No Ragdoll", noRagdoll, "Don't fall over.");
             UIMenuCheckboxItem neverWantedCheckbox = new UIMenuCheckboxItem("Never Wanted", neverWanted, "Nobody has time for annoying cops! Bribe them or deal with it.");
@@ -78,18 +148,23 @@ namespace vMenu.menus
             UIMenuCheckboxItem playerFrozenCheckbox = new UIMenuCheckboxItem("Freeze Yourself", playerFrozen, "Why would you do this...?");
 
             // Wanted level options
-            List<dynamic> wantedLevelList = new List<dynamic> { 0, 1, 2, 3, 4 };
+            List<dynamic> wantedLevelList = new List<dynamic> { "No Cops", 1, 2, 3, 4 };
             UIMenuListItem setWantedLevel = new UIMenuListItem("Set Wanted Level", wantedLevelList, GetPlayerWantedLevel(PlayerId()), "Set the wanted level by selecting a value, and pressing enter.");
+            
             // Player options
             List<dynamic> playerOptionsList = new List<dynamic> { "Heal Player", "Apply Max Armor", "Clean Player", "Dry Player", "Soack Player" };
             UIMenuListItem playerOptions = new UIMenuListItem("Player Options", playerOptionsList, 0, "Select an option and press enter to execute it.");
+            
             // Actions
             List<dynamic> playerActionsList = new List<dynamic> { "Commit Suicide", "Drive To Waypoint", "Drive Wander" };
             UIMenuListItem playerActions = new UIMenuListItem("Player Actions", playerActionsList, 0, "Select an action and press enter to run it, use the cancel button below to stop the driving actions.");
             UIMenuItem cancelActions = new UIMenuItem("Cancel Player Actions", "Click this to cancel any of the driving player actions from the list above.");
-            /// Scenarios (list is in <cref see="PedScenarios.cs">PedScenarios.cs</cref>
+            
+            // Scenarios (list can be found in the PedScenarios class)
             UIMenuListItem playerScenarios = new UIMenuListItem("Scenarios", PedScenarios.Scenarios, 0, "Select a scenario and hit enter to start it. Press it again to cancel it. Selecting another scenario and hitting enter will override the current scenario. Pressing enter again will then stop the scenario.");
+            #endregion
 
+            #region Add items to menu
             // Add all items to the menu.
             menu.AddItem(playerGodModeCheckbox);
             menu.AddItem(invisibleCheckbox);
@@ -105,7 +180,9 @@ namespace vMenu.menus
             menu.AddItem(playerActions);
             menu.AddItem(playerFrozenCheckbox);
             menu.AddItem(playerScenarios);
+            #endregion
 
+            #region Checkbox Changes
             // Handle all checkbox changes.
             menu.OnCheckboxChange += (sender, item, _checked) =>
             {
@@ -113,64 +190,166 @@ namespace vMenu.menus
                 if (item == playerGodModeCheckbox)
                 {
                     godMode = _checked;
+                    Subtitle.Info("God mode has been " + (_checked ? "~g~enabled" : "~r~disabled") + "~b~.");
                 }
                 // Toggle invisibility.
-                if (item == invisibleCheckbox)
+                else if (item == invisibleCheckbox)
                 {
                     invisible = _checked;
+                    Subtitle.Info("Invisibility has been " + (_checked ? "~g~enabled" : "~r~disabled") + "~b~.");
                 }
-                if (item == unlimitedStaminaCheckbox)
+                // Toggle unlimited stamina.
+                else if (item == unlimitedStaminaCheckbox)
                 {
                     unlimitedStamina = _checked;
+                    Subtitle.Info("Unlimited Stamina has been " + (_checked ? "~g~enabled" : "~r~disabled") + "~b~.");
                 }
-                if (item == fastRunCheckbox)
+                // Toggle fast running.
+                else if (item == fastRunCheckbox)
                 {
-                    if (_checked)
-                    {
-                        SetRunSprintMultiplierForPlayer(PlayerId(), 1.49f);
-                    }
-                    else
-                    {
-                        SetRunSprintMultiplierForPlayer(PlayerId(), 1f);
-                    }
+                    SetRunSprintMultiplierForPlayer(PlayerId(), (_checked ? 1.49f : 1.0f));
+                    Subtitle.Info("Fast Running has been " + (_checked ? "~g~enabled" : "~r~disabled") + "~b~.");
                 }
-                if (item == fastSwimCheckbox)
+                // Toggle fast swimming
+                else if (item == fastSwimCheckbox)
                 {
-                    if (_checked)
-                    {
-                        SetSwimMultiplierForPlayer(PlayerId(), 1.49f);
-                    }
-                    else
-                    {
-                        SetSwimMultiplierForPlayer(PlayerId(), 1f);
-                    }
+                    SetSwimMultiplierForPlayer(PlayerId(), (_checked ? 1.49f : 1.0f));
+                    Subtitle.Info("Fast Swimming has been " + (_checked ? "~g~enabled" : "~r~disabled") + "~b~.");
                 }
-                if (item == superJumpCheckbox)
+                // Super jump
+                else if (item == superJumpCheckbox)
                 {
                     superJump = _checked;
+                    Subtitle.Info("Superjump has been " + (_checked ? "~g~enabled" : "~r~disabled") + "~b~.");
                 }
-                if (item == noRagdollCheckbox)
+                // No Ragdoll
+                else if (item == noRagdollCheckbox)
                 {
                     noRagdoll = _checked;
+                    Subtitle.Info("No Ragdoll has been " + (_checked ? "~g~enabled" : "~r~disabled") + "~b~.");
                 }
-                if (item == neverWantedCheckbox)
+                // Never Wanted
+                else if (item == neverWantedCheckbox)
                 {
                     neverWanted = _checked;
+                    Subtitle.Info("Never Wanted has been " + (_checked ? "~g~enabled" : "~r~disabled") + "~b~.");
                 }
-                if (item == everyoneIgnoresPlayerCheckbox)
+                // Everyone Ignores Player
+                else if (item == everyoneIgnoresPlayerCheckbox)
                 {
                     everyoneIgnoresPlayer = _checked;
+                    Subtitle.Info("Everyone Ignores Player has been " + (_checked ? "~g~enabled" : "~r~disabled") + "~b~.");
                 }
-                if (item == playerFrozenCheckbox)
+                // Freeze Player
+                else if (item == playerFrozenCheckbox)
                 {
                     FreezeEntityPosition(PlayerPedId(), playerFrozen);
+                    Subtitle.Info("Freeze Player has been " + (_checked ? "~g~enabled" : "~r~disabled") + "~b~.");
+                }
+            };
+            #endregion
+
+            //menu.OnListChange += (sender, listItem, newIndex) =>
+            //{
+            //    if (listItem == )
+            //};
+
+            menu.OnListSelect += (sender, listItem, index) =>
+            {
+                // Set wanted Level
+                if (listItem == setWantedLevel)
+                {
+                    SetPlayerWantedLevel(PlayerId(), index, false);
+                    SetPlayerWantedLevelNow(PlayerId(), false);
+                }
+                // Player options (healing, cleaning, armor, dry/wet, etc)
+                else if (listItem == playerOptions)
+                {
+                    switch (index) {
+                        case 0:
+                            SetEntityHealth(PlayerPedId(), GetEntityMaxHealth(PlayerPedId()));
+                            Subtitle.Success("Player Healed Successfully.");
+                            break;
+                        case 1:
+                            SetPedArmour(PlayerPedId(), GetPlayerMaxArmour(PlayerId()));
+                            Subtitle.Success("Max Armor Applied Successfully.");
+                            break;
+                        case 2:
+                            ClearPedBloodDamage(PlayerPedId());
+                            Subtitle.Success("Player Successfully Cleaned.");
+                            break;
+                        case 3:
+                            ClearPedWetness(PlayerPedId());
+                            Subtitle.Success("Player Successfully Dried.");
+                            break;
+                        case 4:
+                            SetPedWetnessHeight(PlayerPedId(), 2f);
+                            SetPedWetnessEnabledThisFrame(PlayerPedId());
+                            Subtitle.Success("Player Successfully Soacked.");
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                // Player actions (suicide, driving tasks, etc)
+                else if (listItem == playerActions)
+                {
+                    switch (index)
+                    {
+                        case 0:
+                            SetEntityHealth(PlayerPedId(), 0);
+                            Notify.Success("You killed yourself!");
+                            break;
+                        case 1:
+                            // Todo create drive to wp task.
+                            break;
+                        case 2:
+                            break;
+                            // Todo create drive wander task.
+                        default:
+                            break;
+                    }
+                }
+                else if (listItem == playerScenarios)
+                {
+                    // If they are currently in a scenario, and they select the same scenario, then cancel it.
+                    if (IsPlayingScenario == index)
+                    {
+                        IsPlayingScenario = -1;
+                    }
+                    // Otherwise, start a new scenario.
+                    else
+                    {
+                        // If the player can start a scenario.
+                        if (CanPlayScenarios())
+                        {
+                            // check if they are starting a new scenario for the first time.
+                            if (IsPlayingScenario == -1)
+                            {
+                                // Clear the tasks (any scenarios).
+                                ClearPedTasks(PlayerPedId());
+                                // Start the new scenario.
+                                TaskStartScenarioInPlace(PlayerPedId(), PedScenarios.ScenarioNames[PedScenarios.Scenarios[IsPlayingScenario]], 0, true);
+                            }
+                            IsPlayingScenario = index;
+                        }
+                        else
+                        {
+                            Notify.Alert("You can only start a scenario if you're standing still, and you're not inside any vehicle.");
+                        }
+                    }
                 }
             };
 
+        }
 
-            //FreezeEntityPosition(PlayerPedId(), playerFrozen);
-
-
+        /// <summary>
+        /// Returns true if the player can start a scenario, false if they can't.
+        /// </summary>
+        /// <returns>(bool) True: the player can start a new scenario. False: the player can NOT start a new scenario.</returns>
+        private bool CanPlayScenarios()
+        {
+            return !(IsPedInAnyVehicle(PlayerPedId(), true) || IsPedDeadOrDying(PlayerPedId(), true) || IsPedRagdoll(PlayerPedId()) || IsPedStopped(PlayerPedId()));
         }
     }
 }
