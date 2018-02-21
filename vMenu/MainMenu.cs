0414fcc47aa30c3cb5aef3e7729edb258f8eb8dc
Tom Grobbe <31419184+TomGrobbe@users.noreply.github.com>
2018-02-21 15:58:52 +0100

    Change banner from sprite > UIResRectangle because the sprite is no longer used

--- vMenu/MainMenu.cs
+++ vMenu/MainMenu.cs
@@ -30,7 +30,8 @@ namespace vMenuClient
 
         private BarTimerBar bt = new BarTimerBar("Opening Menu");
         private bool debug = false;
-        public static Sprite BannerSprite { get; private set; } = new Sprite("menubanner", "menu_header", new System.Drawing.PointF(0f, 0f), new System.Drawing.SizeF(0f, 0f), 0f, UnknownColors.SlateGray);
+        //public static Sprite BannerSprite { get; private set; } = new Sprite("menubanner", "menu_header", new System.Drawing.PointF(0f, 0f), new System.Drawing.SizeF(0f, 0f), 0f, UnknownColors.SlateGray);
+        public static UIResRectangle BannerSprite { get; private set; } = new UIResRectangle(new System.Drawing.PointF(0f, 0f), new System.Drawing.SizeF(0f, 0f), UnknownColors.SlateGray);
 
         /// <summary>
         /// Constructor.
