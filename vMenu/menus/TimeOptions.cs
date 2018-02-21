814f5c9cec155b376b6883d66e70b62454ed622e
Tom Grobbe <31419184+TomGrobbe@users.noreply.github.com>
2018-02-21 23:58:34 +0100

    Added and setup all menu classes
    Content will be added later

---
+++ vMenu/menus/TimeOptions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+using NativeUI;
+
+namespace vMenuClient
+{
+    class TimeOptions
+    {
+        // Variables
+        private UIMenu menu;
+        private Notification Notify = MainMenu.Notify;
+        private Subtitles Subtitle = MainMenu.Subtitle;
+        private CommonFunctions cf = MainMenu.cf;
+    }
+}
