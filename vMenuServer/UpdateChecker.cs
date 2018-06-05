0e7aebf2f7719b2309e714597525dfdea5d0fb03
Vespura <31419184+TomGrobbe@users.noreply.github.com>
2018-06-05 15:28:29 +0200

    UpdateChecker: refactoring code

--- vMenuServer/UpdateChecker.cs
+++ vMenuServer/UpdateChecker.cs
@@ -40,42 +40,42 @@ namespace vMenuServer
                 Debug.WriteLine("\r\n[vMenu] Checking for updates.");
 
                 // If the result status = 200 (status code OK) then continue.
-                if (result.status == System.Net.HttpStatusCode.OK)
+                switch (result.status)
                 {
-                    // Get the results
-                    var currentVersion = GetResourceMetadata(GetCurrentResourceName(), "version", 0);
-                    dynamic output = JsonConvert.DeserializeObject<dynamic>(result.content);
-                    string version = output["version"].ToString();
-                    string date = output["date"].ToString();
-                    string changes = output["changes"].ToString() ?? "N/A";
+                    case System.Net.HttpStatusCode.OK:
+                        // Get the results
+                        var currentVersion = GetResourceMetadata(GetCurrentResourceName(), "version", 0);
+                        dynamic output = JsonConvert.DeserializeObject<dynamic>(result.content);
+                        string version = output["version"].ToString();
+                        string date = output["date"].ToString();
+                        string changes = output["changes"].ToString() ?? "N/A";
 
-                    // Output the info.
-                    Debug.WriteLine($"[vMenu] Current version: {currentVersion}");
-                    Debug.WriteLine($"[vMenu] Latest version: {version}");
-                    Debug.WriteLine($"[vMenu] Release Date: {date}");
+                        // Output the info.
+                        Debug.WriteLine($"[vMenu] Current version: {currentVersion}");
+                        Debug.WriteLine($"[vMenu] Latest version: {version}");
+                        Debug.WriteLine($"[vMenu] Release Date: {date}");
 
-                    // If up to date :)
-                    if (currentVersion == version)
-                    {
-                        // yay up to date! :) Snail is happy.
-                        Debug.WriteLine("\r\n[vMenu] You are currently using the latest version, good job!");
+                        // If up to date :)
+                        if (currentVersion == version)
+                        {
+                            // yay up to date! :) Snail is happy.
+                            Debug.WriteLine("\r\n[vMenu] You are currently using the latest version, good job!");
+                        }
+                        // If not up to date :(
+                        else
+                        {
+                            MainServer.UpToDate = false;
 
-                    }
-                    // If not up to date :(
-                    else
-                    {
-                        MainServer.UpToDate = false;
+                            // Snail is sad :(
+                            Debug.WriteLine("\r\n[vMenu] You are NOT using the latest version. Please update to the latest version as soon as possible.");
+                            Debug.WriteLine("[vMenu] Download the latest version here: https://github.com/tomgrobbe/vMenu/releases/ !");
+                            Debug.WriteLine($"[vMenu] New in version {version}: \t{changes}\r\n");
+                        }
 
-                        // Snail is sad :(
-                        Debug.WriteLine("\r\n[vMenu] You are NOT using the latest version. Please update to the latest version as soon as possible.");
-                        Debug.WriteLine("[vMenu] Download the latest version here: https://github.com/tomgrobbe/vMenu/releases/ !");
-                        Debug.WriteLine($"[vMenu] New in version {version}: \t{changes}\r\n");
-                    }
-                }
-                // An error occured, most likely: Vespura's VPS is down.
-                else
-                {
-                    Debug.WriteLine("[vMenu] An error occurred while checking for the latest version. Please try again in a few hours.");
+                        break;
+                    default:
+                        Debug.WriteLine("[vMenu] An error occurred while checking for the latest version. Please try again in a few hours.");
+                        break;
                 }
             }
             // Awww an exception. RIP.
