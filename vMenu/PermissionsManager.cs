c06539a6aa18bdfb90499d097c606517f18a858c
Vespura <31419184+TomGrobbe@users.noreply.github.com>
2018-08-09 21:09:00 +0200

    Hopefully simplified some permission checks to be more efficient, not sure if this really helps, but it's worth a shot.

--- vMenu/PermissionsManager.cs
+++ vMenu/PermissionsManager.cs
@@ -12,33 +12,44 @@ namespace vMenuClient
     {
         public static List<string> Permissions { get; private set; } = new List<string>() { };
 
+        private static bool allowEverything = false;
+
         public static void SetPermission(string permissionName, bool allowed)
         {
             if (allowed)
             {
+                if (permissionName == "Everything")
+                {
+                    allowEverything = true;
+                }
                 Permissions.Add(permissionName);
             }
         }
 
         public static bool IsAllowed(Permission permission)
         {
-            if (Permissions.Contains("Everything"))
+
+            //if (Permissions.Contains("Everything"))
+            if (allowEverything)
             {
-                MainMenu.Cf.Log("Everything allowed, breaking.");
+                MainMenu.Cf.Log($"Everything is allowed, no need to check for \"{permission.ToString()}\" specifically.");
                 return true;
             }
             else
             {
-                var allowed = false;
+                //var allowed = false;
                 if (Permissions.Contains(permission.ToString().Substring(0, 2) + "All"))
                 {
-                    allowed = true;
+                    MainMenu.Cf.Log(".All was allowed.");
+                    //allowed = true;
+                    return true;
                 }
-                if (!allowed)
-                {
-                    allowed = Permissions.Contains(permission.ToString());
-                }
-                return allowed;
+                //if (!allowed)
+                //{
+                //allowed = Permissions.Contains(permission.ToString());
+                //}
+                //return allowed;
+                return Permissions.Contains(permission.ToString());
             }
         }
     }
