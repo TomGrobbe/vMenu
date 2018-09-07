c3939048f29489898c50cab2fa33b02bac6d0cae
Vespura <31419184+TomGrobbe@users.noreply.github.com>
2018-09-07 23:29:39 +0200

    Added more blip sprites (more vehicle sprites will be added in the future)

--- vMenu/BlipInfo.cs
+++ vMenu/BlipInfo.cs
@@ -14,6 +14,38 @@ namespace vMenuClient
         {
             uint model = (uint)GetEntityModel(vehicle);
 
+            if (model == (uint)GetHashKey("rhino"))
+            {
+                return 421; // tank
+            }
+            if (model == (uint)GetHashKey("KHANJALI"))
+            {
+                return 598; // new tank
+            }
+            if (model == (uint)GetHashKey("limo2"))
+            {
+                return 562; // armored limo
+            }
+            if (model == (uint)GetHashKey("TAMPA3") || model == (uint)GetHashKey("BARRAGE") || model == (uint)GetHashKey("HALFTRACK") || model == (uint)GetHashKey("HALFTRACK"))
+            {
+                return 562; // weaponized tampa (and similar small armored/weaponized vehicles)
+            }
+            if (model == (uint)GetHashKey("APC"))
+            {
+                return 558; // apc
+            }
+            if (model == (uint)GetHashKey("DUNE3"))
+            {
+                return 561; // weaponized dune
+            }
+            if (model == (uint)GetHashKey("INSURGENT") || model == (uint)GetHashKey("INSURGENT2") || model == (uint)GetHashKey("INSURGENT3") || model == (uint)GetHashKey("CARACARA"))
+            {
+                return 613; // inurgent pickup
+            }
+            if (model == (uint)GetHashKey("TRAILERSMALL2"))
+            {
+                return 563; // anti aircraft trailer
+            }
             if (IsThisModelABicycle(model) || IsThisModelABike(model))
             {
                 return 226; // (motor)bike
@@ -24,7 +56,7 @@ namespace vMenuClient
             }
             if (model == (uint)VehicleHash.Submersible || model == (uint)VehicleHash.Submersible2)
             {
-                return 308;
+                return 308; // submarine
             }
             if (IsThisModelABoat(model) || IsThisModelAJetski(model))
             {
@@ -32,12 +64,19 @@ namespace vMenuClient
             }
             if (IsThisModelAHeli(model))
             {
-                
                 return 422; // animated helicopter
             }
             if (IsThisModelACar(model))
             {
                 return 225; // regular car
+            }
+            if (model == (uint)VehicleHash.Lazer || model == (uint)VehicleHash.Hydra || model == (uint)VehicleHash.Besra)
+            {
+                return 424; // (fighter)jet
+            }
+            if (IsThisModelAPlane(model))
+            {
+                return 423; // regular plane
             }
             return 225; // regular car
         }
