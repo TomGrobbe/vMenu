5609dc21394e17cb5744fc397519cd5715ee0558
Vespura <31419184+TomGrobbe@users.noreply.github.com>
2018-11-13 20:47:15 +0100

    Trying to see if setting wind speeds makes any difference in wind noise for xmas weather (which currently is ridiculously loud).

--- vMenu/EventManager.cs
+++ vMenu/EventManager.cs
@@ -191,6 +191,9 @@ namespace vMenuClient
                     SetWeatherTypeNowPersist(currentWeatherType);
                 }
                 SetBlackout(blackoutMode);
+                SetWind(0f);
+                SetWindDirection(0f);
+                SetWindSpeed(0f);
             }
         }
 
