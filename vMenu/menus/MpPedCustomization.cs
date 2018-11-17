7b44e9a28c634f1e87f300dbb8764066b47b707b
Vespura <31419184+TomGrobbe@users.noreply.github.com>
2018-11-17 16:32:33 +0100

    Preparations for MP Ped customization menu.

---
+++ vMenu/menus/MpPedCustomization.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CitizenFX.Core;
+using static CitizenFX.Core.Native.API;
+using NativeUI;
+
+namespace vMenuClient
+{
+    public class MpPedCustomization
+    {
+        // Variables
+        private UIMenu menu;
+        private readonly CommonFunctions cf = MainMenu.Cf;
+
+        /// <summary>
+        /// Creates the menu.
+        /// </summary>
+        private void CreateMenu()
+        {
+            // Create the menu.
+            menu = new UIMenu("vMenu", "About vMenu", true)
+            {
+                ScaleWithSafezone = false,
+                MouseControlsEnabled = false,
+                MouseEdgeEnabled = false,
+                ControlDisablingEnabled = false
+            };
+        }
+
+
+        /// <summary>
+        /// Create the menu if it doesn't exist, and then returns it.
+        /// </summary>
+        /// <returns>The Menu</returns>
+        public UIMenu GetMenu()
+        {
+            if (menu == null)
+            {
+                CreateMenu();
+            }
+            return menu;
+        }
+    }
+}
