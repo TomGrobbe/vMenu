d8feefab68efb13fa71c7d4ccd07a5ff4a8064ca
Vespura <31419184+TomGrobbe@users.noreply.github.com>
2018-11-23 17:12:41 +0100

    Create the MP Peds Menu (WIP)

--- vMenu/menus/MpPedCustomization.cs
+++ vMenu/menus/MpPedCustomization.cs
@@ -14,6 +14,27 @@ namespace vMenuClient
         // Variables
         private UIMenu menu;
         private readonly CommonFunctions cf = MainMenu.Cf;
+        public UIMenu createMaleMenu = new UIMenu("Create Character", "Create A Male Character", true)
+        {
+            ScaleWithSafezone = false,
+            MouseControlsEnabled = false,
+            MouseEdgeEnabled = false,
+            ControlDisablingEnabled = false
+        };
+        public UIMenu createFemaleMenu = new UIMenu("Create Character", "Create A Female Character", true)
+        {
+            ScaleWithSafezone = false,
+            MouseControlsEnabled = false,
+            MouseEdgeEnabled = false,
+            ControlDisablingEnabled = false
+        };
+        public UIMenu savedCharactersMenu = new UIMenu("Saved Characters", "Manage Saved MP Characters", true)
+        {
+            ScaleWithSafezone = false,
+            MouseControlsEnabled = false,
+            MouseEdgeEnabled = false,
+            ControlDisablingEnabled = false
+        };
 
         /// <summary>
         /// Creates the menu.
@@ -28,6 +49,63 @@ namespace vMenuClient
                 MouseEdgeEnabled = false,
                 ControlDisablingEnabled = false
             };
+
+            UIMenuItem createMale = new UIMenuItem("Create Male Character", "Create a new male character.");
+            createMale.SetRightLabel("→→→");
+
+            UIMenuItem savedCharacters = new UIMenuItem("Saved Characters", "Spawn, edit or delete your existing saved multiplayer characters.");
+            savedCharacters.SetRightLabel("→→→");
+
+            UIMenuItem createFemale = new UIMenuItem("Create Female Character", "Create a new female character.");
+            createFemale.SetRightLabel("→→→");
+
+            MainMenu.Mp.Add(createMaleMenu);
+            MainMenu.Mp.Add(createFemaleMenu);
+            MainMenu.Mp.Add(savedCharactersMenu);
+
+            CreateMaleCreatorMenu();
+            CreateFemaleCreatorMenu();
+            CreateSavedPedsMenu();
+
+            menu.AddItem(createMale);
+            menu.BindMenuToItem(createMaleMenu, createMale);
+
+            menu.AddItem(createFemale);
+            menu.BindMenuToItem(createFemaleMenu, createFemale);
+
+            menu.AddItem(savedCharacters);
+            menu.BindMenuToItem(savedCharactersMenu, savedCharacters);
+
+            menu.UpdateScaleform();
+
+            menu.AddInstructionalButton(new InstructionalButton(Control.MoveLeftRight, "Turn Head"));
+            createMaleMenu.AddInstructionalButton(new InstructionalButton(Control.MoveLeftRight, "Turn Head"));
+            createFemaleMenu.AddInstructionalButton(new InstructionalButton(Control.MoveLeftRight, "Turn Head"));
+
+            createMaleMenu.UpdateScaleform();
+            createFemaleMenu.UpdateScaleform();
+            savedCharactersMenu.UpdateScaleform();
+        }
+
+        /// <summary>
+        /// Creates the saved mp characters menu.
+        /// </summary>
+        private void CreateSavedPedsMenu()
+        {
+        }
+
+        /// <summary>
+        /// Creates the female creator menu.
+        /// </summary>
+        private void CreateFemaleCreatorMenu()
+        {
+        }
+
+        /// <summary>
+        /// Creates the male creator menu.
+        /// </summary>
+        private void CreateMaleCreatorMenu()
+        {
         }
 
 
@@ -41,7 +119,6 @@ namespace vMenuClient
             {
                 CreateMenu();
             }
-            //Debug.WriteLine(Newtonsoft.Json.JsonConvert.SerializeObject(cf.MpPedDataManager.GetHeadBlendData()));
             return menu;
         }
 
