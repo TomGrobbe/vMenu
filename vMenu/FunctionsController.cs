c298595aca9484885406cdd1ba660241fbe80841
Vespura <31419184+TomGrobbe@users.noreply.github.com>
2019-01-12 01:34:53 +0100

    Fix null exception if weapon loadouts menu is not allowed whenever the ped respawns

--- vMenu/FunctionsController.cs
+++ vMenu/FunctionsController.cs
@@ -1588,7 +1588,7 @@ namespace vMenuClient
                         }
                     }
 
-                    if (MainMenu.MiscSettingsMenu.RestorePlayerWeapons && IsAllowed(Permission.MSRestoreWeapons) || (MainMenu.WeaponLoadoutsMenu.WeaponLoadoutsSetLoadoutOnRespawn && IsAllowed(Permission.WLEquipOnRespawn)))
+                    if (MainMenu.MiscSettingsMenu != null && MainMenu.MiscSettingsMenu.RestorePlayerWeapons && IsAllowed(Permission.MSRestoreWeapons) || (MainMenu.WeaponLoadoutsMenu != null && MainMenu.WeaponLoadoutsMenu.WeaponLoadoutsSetLoadoutOnRespawn && IsAllowed(Permission.WLEquipOnRespawn)))
                     {
                         await SpawnWeaponLoadoutAsync("vmenu_temp_weapons_loadout_before_respawn", true);
                         Log("weapons restored, deleting kvp");
