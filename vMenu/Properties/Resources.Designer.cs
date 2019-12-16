408b7a620c7f00dc41c6ee2d84093c6479e67883
Vespura <31419184+TomGrobbe@users.noreply.github.com>
2019-12-16 23:52:26 +0100

    Update .csproj formats and use NuGet for MenuAPI

--- vMenu/Properties/Resources.Designer.cs
+++ vMenu/Properties/Resources.Designer.cs
@@ -22,7 +22,7 @@ namespace vMenuClient.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources {
+    public class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace vMenuClient.Properties {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("vMenuClient.Properties.Resources", typeof(Resources).Assembly);
@@ -51,7 +51,7 @@ namespace vMenuClient.Properties {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
@@ -84,7 +84,7 @@ namespace vMenuClient.Properties {
         ///    &quot;collectionName&quot;: &quot;mpAirraces_overlays&quot;,
         ///    &quot;zoneId&quot;: 0 [rest of string was truncated]&quot;;.
         /// </summary>
-        internal static string overlays {
+        public static string overlays {
             get {
                 return ResourceManager.GetString("overlays", resourceCulture);
             }
