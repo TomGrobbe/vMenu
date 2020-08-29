d93326c7215330e0d4ae48f8586e05318aa6cffe
Vespura <31419184+TomGrobbe@users.noreply.github.com>
2020-08-29 17:24:31 +0200

    Always push build file to Discord, hopefully fix PR's
    be quiet curl
    
    
    Test simplefied webhooks
    
    
    Why is this failing
    
    
    get output
    
    
    might fix it
    
    
    fix appveyor
    
    
    Use non-testing webhooks

---
+++ appveyor/on_success.bat
@@ -0,0 +1,10 @@
+if not defined WEBHOOK_URL goto file
+
+curl -H "Content-Type: application/x-www-form-urlencoded" -d "success=true&commit_id=%APPVEYOR_REPO_COMMIT:~0,7%&project_name=%APPVEYOR_PROJECT_NAME%&build_url=%APPVEYOR_URL%/project/%APPVEYOR_ACCOUNT_NAME%/%APPVEYOR_PROJECT_SLUG%/builds/%APPVEYOR_BUILD_ID%&author=Vespura&commit_url=https://github.com/%APPVEYOR_REPO_NAME%/commit/%APPVEYOR_REPO_COMMIT%&commit_message=%APPVEYOR_REPO_COMMIT_MESSAGE%&build_name=%APPVEYOR_PROJECT_NAME%-%APPVEYOR_BUILD_ID%-%APPVEYOR_BUILD_NUMBER%" %WEBHOOK_URL%
+
+:file
+
+if not defined DISCORD_FILE_WEBHOOK goto end
+curl -s -o nul -F "file=@vMenu-%VERSION_NAME%.zip" %DISCORD_FILE_WEBHOOK%
+
+:end
