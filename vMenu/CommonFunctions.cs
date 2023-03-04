d3cda86a235345a09c120237599735ca225bd0c8
Vespura <31419184+TomGrobbe@users.noreply.github.com>
2023-03-04 15:07:24 +0100

    Update dependencies, add .editorconfig for basic code editor style guide, cleanup some usings.

--- vMenu/CommonFunctions.cs
+++ vMenu/CommonFunctions.cs
@@ -1,13 +1,16 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using MenuAPI;
-using Newtonsoft.Json;
+
 using CitizenFX.Core;
-using static CitizenFX.Core.UI.Screen;
+
+using MenuAPI;
+
+using Newtonsoft.Json;
+
 using static CitizenFX.Core.Native.API;
+using static CitizenFX.Core.UI.Screen;
 using static vMenuShared.PermissionsManager;
 
 namespace vMenuClient
