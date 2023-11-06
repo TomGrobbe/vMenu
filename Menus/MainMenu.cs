6536c647e9ba4c945d2de4003635d109ff5e187b
Xd_Golden_Tiger <68713969+XdGoldenTigerOfficial@users.noreply.github.com>
2023-11-07 05:43:39 +1300

    Add files via upload

---
+++ Menus/MainMenu.cs
@@ -0,0 +1,75 @@
+﻿// System Libraries //
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+// CitizenFX Libraries //
+using CitizenFX.Core;
+using CitizenFX.Core.Native;
+using static CitizenFX.FiveM.Native.Natives;
+
+// ScaleformUI Libraries //
+using ScaleformUI;
+using ScaleformUI.Elements;
+using ScaleformUI.LobbyMenu;
+using ScaleformUI.Menu;
+using ScaleformUI.PauseMenu;
+using ScaleformUI.PauseMenus;
+using ScaleformUI.Radial;
+using ScaleformUI.Radio;
+using ScaleformUI.Scaleforms;
+using vMenu.Client.Functions;
+
+namespace vMenu.Client.Menus
+{
+    public class MainMenu : BaseScript
+    {
+        public static MenuFunctions MenuFunctions = new MenuFunctions();
+
+        private static UIMenu mainMenu;
+
+        public MainMenu()
+        {
+            
+        }
+
+        public static UIMenu Menu()
+        {
+            mainMenu = new UIMenu(Main.MenuBanner.BannerTitle, "Main Men", MenuFunctions.GetMenuOffset(), Main.MenuBanner.TextureDictionary, Main.MenuBanner.TextureName, false, true)
+            {
+                MaxItemsOnScreen = 7,
+                BuildingAnimation = MenuBuildingAnimation.NONE,
+                ScrollingType = ScrollingType.CLASSIC,
+                Enabled3DAnimations = false,
+                MouseControlsEnabled = false,
+                ControlDisablingEnabled = false,
+            };
+
+            Main.Menus.Add(mainMenu);
+
+            UIMenuItem onlinePlayers = new UIMenuItem("Online Players", "All currently connected players");
+            onlinePlayers.SetRightLabel(">>>");
+
+            UIMenuItem miscOptions = new UIMenuItem("Misc. Options", "Miscellaneous vMenu options/settings can be configured here. You can also save your settings in this menu");
+            miscOptions.SetRightLabel(">>>");
+
+            mainMenu.AddItem(onlinePlayers);
+            mainMenu.AddItem(miscOptions);
+
+            onlinePlayers.Activated += (sender, i) =>
+            {
+                sender.SwitchTo(OnlinePlayersMenu.Menu(), inheritOldMenuParams: true);
+            };
+
+            miscOptions.Activated += (sender, i) =>
+            {
+                sender.SwitchTo(MiscOptionsMenu.Menu(), inheritOldMenuParams: true);
+            };
+
+            return mainMenu;
+        }
+    }
+}
---
+++ Menus/OnlinePlayersMenu.cs
@@ -0,0 +1,90 @@
+﻿using CitizenFX.Core;
+using CitizenFX.FiveM;
+using ScaleformUI.Menu;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using vMenu.Client.Functions;
+using static CitizenFX.FiveM.Native.Natives;
+using static CitizenFX.FiveM.PlayerList;
+
+namespace vMenu.Client.Menus
+{
+    public class OnlinePlayersMenu : BaseScript
+    {
+        public static MenuFunctions MenuFunctions = new MenuFunctions();
+
+        private static UIMenu onlinePlayersMenu = null;
+
+        private static int updatingPlayerList;
+
+        public OnlinePlayersMenu()
+        {
+            
+        }
+
+        public static UIMenu Menu()
+        {
+            onlinePlayersMenu = new UIMenu(Main.MenuBanner.BannerTitle, "Online Players", MenuFunctions.GetMenuOffset(), Main.MenuBanner.TextureDictionary, Main.MenuBanner.TextureName, false, true);
+            UIMenuSeparatorItem onlinePlayerq = new UIMenuSeparatorItem("No Players Online", false);
+            onlinePlayersMenu.AddItem(onlinePlayerq);
+
+            Main.Menus.Add(onlinePlayersMenu);
+
+            return onlinePlayersMenu;
+        }
+
+        public static async Coroutine UpdateOnlinePlayers()
+        {
+            PlayerList PlayersList = Players;
+            updatingPlayerList = PlayersList.Count();
+
+            async void UpdateStuffAsync()
+            {
+                onlinePlayersMenu.MenuItems.Clear();
+                onlinePlayersMenu.Windows.Clear();
+
+                foreach (Player player in PlayersList.OrderBy(a => a.Name))
+                {
+                    UIMenuItem onlinePlayer = new UIMenuItem(player.Name, "Click to view the options for this player");
+
+                    int mugshot = RegisterPedheadshot(player.Character.Handle);
+
+                    while (!IsPedheadshotReady(mugshot)) await WaitUntilNextFrame();
+
+                    string mugtxd = GetPedheadshotTxdString(mugshot);
+
+                    UIMenuDetailsWindow playerDetails = new UIMenuDetailsWindow($"~h~{player.Name}~h~", $"Server ID: {player.ServerId}\nLocal ID: {player.Handle}", $"Steam ID: test\nDiscord ID: yeet", new UIDetailImage()
+                    {
+                        Txd = mugtxd,
+                        Txn = mugtxd,
+                        Size = new Size(60, 60)
+                    });
+
+                    onlinePlayersMenu.AddWindow(playerDetails);
+                    onlinePlayersMenu.AddItem(onlinePlayer);
+
+                    onlinePlayersMenu.Visible = false;
+                    onlinePlayersMenu.Visible = true;
+
+                    updatingPlayerList--;
+                }
+            }
+
+            Debug.WriteLine(updatingPlayerList);
+
+            UpdateStuffAsync();
+
+            while (updatingPlayerList > 0)
+            {
+                await Wait(100);
+            }
+
+            await Wait(5000);
+        }
+    }
+}
