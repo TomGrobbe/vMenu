15b7804d7e9ed08cf4a498b714d19c06bd8fb312
Ricky Merc <fizzingwizzbie@gmail.com>
2023-11-09 08:37:55 -0500

    Changes
    Added Discord Rich Presence, added KeyMapping.json (allows server administration to add/change/remove KeyMapping from vMenu, and added themes

--- vMenu.Client/KeyMappings/Commands.cs
+++ vMenu.Client/KeyMappings/Commands.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 // CitizenFX Libraries //
 using CitizenFX.Core;
@@ -13,16 +14,42 @@ using static CitizenFX.Core.Native.API;
 // vMenu Namespaces //
 using vMenu.Client.Menus;
 using ScaleformUI.Menu;
+using vMenu.Client.Functions;
 
 namespace vMenu.Client.KeyMappings
 {
     public class Commands : BaseScript
     {
+        public static bool NoClipEnabled { get { return Functions.NoClip.IsNoclipActive(); } set { Functions.NoClip.SetNoclipActive(value); } }
+
+        public struct CommandStructure
+        {
+            public string Description;
+            public string ControlType;
+            public string Button;
+
+            public CommandStructure(string Description, string ControlType, string Button)
+            {
+                this.Description = Description;
+                this.ControlType = ControlType;
+                this.Button = Button;
+            }
+        }
         public Commands()
         {
-            RegisterKeyMapping("vMenu:OpenMenu", "Open vMenu Toggle", "keyboard", "M");
-        }
+            string JsonData = LoadResourceFile(GetCurrentResourceName(), "KeyMapping.json") ?? "{}";
+            var JsonCommand = JsonConvert.DeserializeObject<Dictionary<string, CommandStructure>>(JsonData);
 
+            foreach (var CommandData in JsonCommand)
+            {
+                RegisterKeyMapping(CommandData.Key, CommandData.Value.Description, CommandData.Value.ControlType, CommandData.Value.Button);
+            }
+        }
+        [Command("vMenu:NoClip")]
+        private void NoClip()
+        {
+            NoClipEnabled = !NoClipEnabled;
+        }
         [Command("vMenu:OpenMenu")]
         private void MenuOpen()
         {
