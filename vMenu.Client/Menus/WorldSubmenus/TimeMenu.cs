447a9a9116d1734678fa60a1b9d5c950407c76a2
Ricky Merc <fizzingwizzbie@gmail.com>
2023-11-20 14:33:09 -0500

    Updates
    Added substitutes to rich presence, added values in the time options to change time, added smooth time changing, and more

--- vMenu.Client/Menus/WorldSubmenus/TimeMenu.cs
+++ vMenu.Client/Menus/WorldSubmenus/TimeMenu.cs
@@ -21,6 +21,7 @@ namespace vMenu.Client.Menus.WorldSubmenus
     public class TimeOptionsMenu
     {
         private static UIMenu timeOptionsMenu = null;
+        public static UIMenuItem freezeTimeToggle;
 
         public TimeOptionsMenu()
         {
@@ -28,18 +29,123 @@ namespace vMenu.Client.Menus.WorldSubmenus
 
             timeOptionsMenu = new Objects.vMenu(MenuLanguage.Subtitle ?? "Time Options").Create();
 
-            UIMenuSeparatorItem button = new UIMenuSeparatorItem("Under Construction!", false)
+
+            freezeTimeToggle = new UIMenuItem("Freeze/Unfreeze Time", "Enable or disable time freezing.", MenuSettings.Colours.Items.BackgroundColor, MenuSettings.Colours.Items.HighlightColor);
+            UIMenuItem earlymorning = new UIMenuItem("Early Morning", "Set the time to 06:00.", MenuSettings.Colours.Items.BackgroundColor, MenuSettings.Colours.Items.HighlightColor);
+            earlymorning.SetRightLabel("06:00");
+            UIMenuItem morning = new UIMenuItem("Morning", "Set the time to 09:00.", MenuSettings.Colours.Items.BackgroundColor, MenuSettings.Colours.Items.HighlightColor);
+            morning.SetRightLabel("09:00");
+            UIMenuItem noon = new UIMenuItem("Noon", "Set the time to 12:00.", MenuSettings.Colours.Items.BackgroundColor, MenuSettings.Colours.Items.HighlightColor);
+            noon.SetRightLabel("12:00");
+            UIMenuItem earlyafternoon = new UIMenuItem("Early Afternoon", "Set the time to 15:00.", MenuSettings.Colours.Items.BackgroundColor, MenuSettings.Colours.Items.HighlightColor);
+            earlyafternoon.SetRightLabel("15:00");
+            UIMenuItem afternoon = new UIMenuItem("Afternoon", "Set the time to 18:00.", MenuSettings.Colours.Items.BackgroundColor, MenuSettings.Colours.Items.HighlightColor);
+            afternoon.SetRightLabel("18:00");
+            UIMenuItem evening = new UIMenuItem("Evening", "Set the time to 21:00.", MenuSettings.Colours.Items.BackgroundColor, MenuSettings.Colours.Items.HighlightColor);
+            evening.SetRightLabel("21:00");
+            UIMenuItem midnight = new UIMenuItem("Midnight", "Set the time to 00:00.", MenuSettings.Colours.Items.BackgroundColor, MenuSettings.Colours.Items.HighlightColor);
+            midnight.SetRightLabel("00:00");
+            UIMenuItem night = new UIMenuItem("Night", "Set the time to 03:00.", MenuSettings.Colours.Items.BackgroundColor, MenuSettings.Colours.Items.HighlightColor);
+            night.SetRightLabel("03:00");
+            
+            var hours = new List<dynamic>() { "00", "01", "02", "03", "04", "05", "06", "07", "08", "09" };
+            var minutes = new List<dynamic>() { "00", "01", "02", "03", "04", "05", "06", "07", "08", "09" };
+            for (var i = 10; i < 60; i++)
             {
-                MainColor = MenuSettings.Colours.Spacers.BackgroundColor,
-                HighlightColor = MenuSettings.Colours.Spacers.HighlightColor,
-                HighlightedTextColor = MenuSettings.Colours.Spacers.HighlightedTextColor,
-                TextColor = MenuSettings.Colours.Spacers.TextColor
+                if (i < 24)
+                {
+                    hours.Add(i.ToString());
+                }
+                minutes.Add(i.ToString());
+            }
+            UIMenuListItem hour = new UIMenuListItem("Hour", hours, 0, "Hour", MenuSettings.Colours.Items.BackgroundColor, MenuSettings.Colours.Items.HighlightColor);
+            UIMenuListItem minute = new UIMenuListItem("Minute", minutes, 0, "Minute", MenuSettings.Colours.Items.BackgroundColor, MenuSettings.Colours.Items.HighlightColor);
+            
+
+            timeOptionsMenu.OnListSelect += (sender, item, itemIndex) =>
+            {
+                var updatedminute = MathUtil.Clamp(Convar.GetSettingsInt("vmenu_current_minute"), 0, 59);
+                var updatedhour = MathUtil.Clamp(Convar.GetSettingsInt("vmenu_current_hour"), 0, 23);
+                var updatedfreeze = Convar.GetSettingsBool("vmenu_freeze_time");
+                
+                if (item == hour)
+                {
+                    updatedhour = item.Index;
+                    BaseScript.TriggerServerEvent("vMenu:UpdateServerTime", updatedhour, updatedminute, updatedfreeze);
+                    Subtitle.Info($"Time set to ~y~{(updatedhour < 10 ? $"0{updatedhour}" : updatedhour.ToString())}~s~:~y~" + $"{(updatedminute < 10 ? $"0{updatedminute}" : updatedminute.ToString())}~s~.");
+                }
+                else if (item == minute)
+                {
+                    updatedminute = item.Index;
+                    BaseScript.TriggerServerEvent("vMenu:UpdateServerTime", updatedhour, updatedminute, updatedfreeze);
+                    Subtitle.Info($"Time set to ~y~{(updatedhour < 10 ? $"0{updatedhour}" : updatedhour.ToString())}~s~:~y~" + $"{(updatedminute < 10 ? $"0{updatedminute}" : updatedminute.ToString())}~s~.");
+                }
             };
 
-            button.LabelFont = new ItemFont(Main.CustomFontName, Main.CustomFontId);
+            timeOptionsMenu.OnItemSelect += (sender, item, index) =>
+            {
+                var updatedminute = MathUtil.Clamp(Convar.GetSettingsInt("vmenu_current_minute"), 0, 59);
+                var updatedhour = MathUtil.Clamp(Convar.GetSettingsInt("vmenu_current_hour"), 0, 23);
+                var updatedfreeze = Convar.GetSettingsBool("vmenu_freeze_time");
+                if (item == freezeTimeToggle)
+                {
+                    BaseScript.TriggerServerEvent("vMenu:UpdateServerTime", updatedhour, updatedminute, !updatedfreeze);
+                    Subtitle.Info($"Time will now {(updatedfreeze ? "~y~continue" : "~o~freeze")}~s~.");
+                }
+                else if (item == earlymorning)
+                {
+                    BaseScript.TriggerServerEvent("vMenu:UpdateServerTime", 6, 0, updatedfreeze);
+                    Subtitle.Info($"Time set to ~y~06~s~:~y~" + $"00~s~.");
+                }
+                else if (item == morning)
+                {
+                    BaseScript.TriggerServerEvent("vMenu:UpdateServerTime", 9, 0, updatedfreeze);
+                    Subtitle.Info($"Time set to ~y~09~s~:~y~" + $"00~s~.");
+                }
+                else if (item == noon)
+                {
+                    BaseScript.TriggerServerEvent("vMenu:UpdateServerTime", 12, 0, updatedfreeze);
+                    Subtitle.Info($"Time set to ~y~12~s~:~y~" + $"00~s~.");
+                }
+                else if (item == earlyafternoon)
+                {
+                    BaseScript.TriggerServerEvent("vMenu:UpdateServerTime", 15, 0, updatedfreeze);
+                    Subtitle.Info($"Time set to ~y~15~s~:~y~" + $"00~s~.");
+                }
+                else if (item == afternoon)
+                {
+                    BaseScript.TriggerServerEvent("vMenu:UpdateServerTime", 18, 0, updatedfreeze);
+                    Subtitle.Info($"Time set to ~y~18~s~:~y~" + $"00~s~.");
+                }
+                else if (item == evening)
+                {
+                    BaseScript.TriggerServerEvent("vMenu:UpdateServerTime", 21, 0, updatedfreeze);
+                    Subtitle.Info($"Time set to ~y~21~s~:~y~" + $"00~s~.");
+                }
+                else if (item == midnight)
+                {
+                    BaseScript.TriggerServerEvent("vMenu:UpdateServerTime", 0, 0, updatedfreeze);
+                    Subtitle.Info($"Time set to ~y~00~s~:~y~" + $"00~s~.");
+                }
+                else if (item == night)
+                {
+                    BaseScript.TriggerServerEvent("vMenu:UpdateServerTime", 3, 0, updatedfreeze);
+                    Subtitle.Info($"Time set to ~y~03~s~:~y~" + $"00~s~.");
+                }
+                Debug.WriteLine($"{item}");
+            };
 
-            timeOptionsMenu.AddItem(button);
-
+            timeOptionsMenu.AddItem(hour);
+            timeOptionsMenu.AddItem(minute);
+            timeOptionsMenu.AddItem(freezeTimeToggle);
+            timeOptionsMenu.AddItem(earlymorning);
+            timeOptionsMenu.AddItem(morning);
+            timeOptionsMenu.AddItem(noon);
+            timeOptionsMenu.AddItem(earlyafternoon);
+            timeOptionsMenu.AddItem(afternoon);
+            timeOptionsMenu.AddItem(evening);
+            timeOptionsMenu.AddItem(midnight);
+            timeOptionsMenu.AddItem(night);
             Main.Menus.Add(timeOptionsMenu);
         }
 
--- vMenu.Server/Functions/TimeWeather.cs
+++ vMenu.Server/Functions/TimeWeather.cs
@@ -26,19 +26,15 @@ namespace vMenu.Server.Functions
             get { return MathUtil.Clamp(Convar.GetSettingsInt("vmenu_current_minute"), 0, 59); }
             set { SetConvarReplicated("vmenu_current_minute", MathUtil.Clamp(value, 0, 59).ToString()); }
         }
-        private int MinuteClockSpeed
-        {
-            get
-            {
-                var value = Convar.GetSettingsInt("vmenu_ingame_minute_duration");
-                if (value < 100)
-                {
-                    value = 2000;
-                }
 
-                return value;
-            }
+        private static int MinuteClockSpeed => MathUtil.Clamp(Convar.GetSettingsInt("vmenu_ingame_minute_duration"), 100, 2000);
+
+        private bool TimeChange
+        {
+            get { return Convar.GetSettingsBool("vmenu_smooth_change"); }
+            set { SetConvarReplicated("vmenu_smooth_change", value.ToString().ToLower()); }
         }
+
         private bool FreezeTime
         {
             get { return Convar.GetSettingsBool("vmenu_freeze_time"); }
@@ -51,12 +47,18 @@ namespace vMenu.Server.Functions
         /// <param name="newHours"></param>
         /// <param name="newMinutes"></param>
         /// <param name="freezeTimeNew"></param>
+
+        private int SetCurrentHours = 0;
+        private int SetCurrentMinutes = 0;
+
+
         [EventHandler("vMenu:UpdateServerTime")]
         internal void UpdateTime(int newHours, int newMinutes, bool freezeTimeNew)
         {
-            CurrentHours = newHours;
-            CurrentMinutes = newMinutes;
+            SetCurrentHours = newHours;
+            SetCurrentMinutes = newMinutes;
             FreezeTime = freezeTimeNew;
+            TimeChange = true;
         }
         /// <summary>
         /// Loop used for syncing and keeping track of the time in-game.
@@ -64,6 +66,7 @@ namespace vMenu.Server.Functions
         /// <returns></returns>
         private async Task TimeLoop()
         {
+            var tempspeed = MinuteClockSpeed;
             if (IsServerTimeSynced)
             {
                 var currentTime = DateTime.Now;
@@ -75,26 +78,110 @@ namespace vMenu.Server.Functions
             }
             else
             {
-                if (!FreezeTime)
+                if (TimeChange)
                 {
-                    if ((CurrentMinutes + 1) > 59)
+                    tempspeed = 1;
+
+                    var nval = ((SetCurrentHours - CurrentHours) * -1);
+                    if (nval > 0)
                     {
-                        CurrentMinutes = 0;
-                        if ((CurrentHours + 1) > 23)
-                        {
-                            CurrentHours = 0;
-                        }
-                        else
-                        {
-                            CurrentHours++;
-                        }
+                        nval = (nval + 12) * -1;
                     }
                     else
                     {
-                        CurrentMinutes++;
+                        nval = nval + 12;
+                    }
+
+                    if (nval == 0)
+                    {
+                        if ((CurrentMinutes + 1) > 59)
+                        {
+                            CurrentMinutes = 0;
+                            if ((CurrentHours + 1) > 23)
+                            {
+                                CurrentHours = 0;
+                            }
+                            else
+                            {
+                                CurrentHours ++;
+                            }
+                        }
+                        else
+                        {
+                            CurrentMinutes = MathUtil.Clamp(CurrentMinutes + 5, 0, 59);
+                        }    
+                    }
+                    else
+                    {
+                        if (nval < 0)
+                        {
+                            if (CurrentMinutes < 1)
+                            {
+                                CurrentMinutes = 59;
+                                if (CurrentHours < 1)
+                                {
+                                    CurrentHours = 23;
+                                }
+                                else
+                                {
+                                    CurrentHours = CurrentHours - 1;
+                                }
+                            }
+                            else
+                            {
+                                CurrentMinutes = MathUtil.Clamp(CurrentMinutes - 5, 0, 59);
+                            }
+                        }
+                        else
+                        {
+                            if ((CurrentMinutes + 1) > 59)
+                            {
+                                CurrentMinutes = 0;
+                                if ((CurrentHours + 1) > 23)
+                                {
+                                    CurrentHours = 0;
+                                }
+                                else
+                                {
+                                    CurrentHours ++;
+                                }
+                            }
+                            else
+                            {
+                                CurrentMinutes = MathUtil.Clamp(CurrentMinutes + 5, 0, 59);
+                            }
+                        }
+                    }
+                    if ((CurrentMinutes >= SetCurrentMinutes) && (CurrentHours == SetCurrentHours))
+                    {
+                        CurrentHours = SetCurrentHours;
+                        CurrentMinutes = SetCurrentMinutes;
+                        TimeChange = false;
                     }
                 }
-                await Delay(MinuteClockSpeed);
+                else
+                {
+                    if (!FreezeTime)
+                    {
+                        if ((CurrentMinutes + 1) > 59)
+                        {
+                            CurrentMinutes = 0;
+                            if ((CurrentHours + 1) > 23)
+                            {
+                                CurrentHours = 0;
+                            }
+                            else
+                            {
+                                CurrentHours++;
+                            }
+                        }
+                        else
+                        {
+                            CurrentMinutes++;
+                        }
+                    }
+                }
+                await Delay(tempspeed);
             }
         }        
     }
