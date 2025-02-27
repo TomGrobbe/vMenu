2a11f6b8843bf3be5da2eb07772b9a60eb38a31b
Tom Grobbe <t.grobbe@spotonmedics.nl>
2025-02-27 12:17:55 +0100

    test ps

--- appveyor/on_success.ps1
+++ appveyor/on_success.ps1
@@ -1,23 +1,24 @@
-if (-not $env:WEBHOOK_URL) { goto file }
+# Check if $env:WEBHOOK_URL is defined
 
-Invoke-RestMethod -Uri $env:WEBHOOK_URL -Method Post -ContentType "application/x-www-form-urlencoded" -Body @{
-    success = "true"
-    commit_id = $env:APPVEYOR_REPO_COMMIT.Substring(0, 7)
-    project_name = $env:APPVEYOR_PROJECT_NAME
-    build_url = "$env:APPVEYOR_URL/project/$env:APPVEYOR_ACCOUNT_NAME/$env:APPVEYOR_PROJECT_SLUG/builds/$env:APPVEYOR_BUILD_ID"
-    author = "Vespura"
-    commit_url = "https://github.com/$env:APPVEYOR_REPO_NAME/commit/$env:APPVEYOR_REPO_COMMIT"
-    commit_message = $env:APPVEYOR_REPO_COMMIT_MESSAGE
-    build_name = "$env:APPVEYOR_PROJECT_NAME-$env:APPVEYOR_BUILD_ID-$env:APPVEYOR_BUILD_NUMBER"
+if ($env:WEBHOOK_URL) {
+    Invoke-RestMethod -Uri $env:WEBHOOK_URL -Method Post -ContentType "application/x-www-form-urlencoded" -Body @{
+        success = "true"
+        commit_id = $env:APPVEYOR_REPO_COMMIT.Substring(0, 7)
+        project_name = $env:APPVEYOR_PROJECT_NAME
+        build_url = "$env:APPVEYOR_URL/project/$env:APPVEYOR_ACCOUNT_NAME/$env:APPVEYOR_PROJECT_SLUG/builds/$env:APPVEYOR_BUILD_ID"
+        author = "Vespura"
+        commit_url = "https://github.com/$env:APPVEYOR_REPO_NAME/commit/$env:APPVEYOR_REPO_COMMIT"
+        commit_message = $env:APPVEYOR_REPO_COMMIT_MESSAGE
+        build_name = "$env:APPVEYOR_PROJECT_NAME-$env:APPVEYOR_BUILD_ID-$env:APPVEYOR_BUILD_NUMBER"
+    }
+    Write-Output "Success webhook sent."
+} else {
+    Write-Output "No webhook URL defined, skipping webhook."
 }
-Write-Output "Success webhook sent."
 
-:file
-Write-Output "Past :file"
-
-if (-not $env:DISCORD_FILE_WEBHOOK) { goto end }
-Invoke-RestMethod -Uri $env:DISCORD_FILE_WEBHOOK -Method Post -InFile "vMenu-$env:VERSION_NAME.zip" -ContentType "multipart/form-data"
-Write-Output "File webhook sent."
-
-:end
-Write-Output "Past :end"
+if ($env:DISCORD_FILE_WEBHOOK) {
+    Invoke-RestMethod -Uri $env:DISCORD_FILE_WEBHOOK -Method Post -InFile "vMenu-$env:VERSION_NAME.zip" -ContentType "multipart/form-data"
+    Write-Output "File webhook sent."
+} else {
+    Write-Output "No file webhook URL defined, skipping file webhook."
+}
