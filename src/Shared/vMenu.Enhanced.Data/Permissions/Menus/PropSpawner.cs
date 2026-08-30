0b527cad9b8f4a9c5ee274a325836beed11599a5
MrPizza <5529325+sir-lord-pizza@users.noreply.github.com>
2026-08-30 17:03:14 +0200

    tweak(permissions): make entity spawner menu staff only by default.

--- src/Shared/vMenu.Enhanced.Data/Permissions/Menus/PropSpawner.cs
+++ src/Shared/vMenu.Enhanced.Data/Permissions/Menus/PropSpawner.cs
@@ -3,18 +3,25 @@ namespace vMenu.Enhanced.Data.Permissions.Menus;
 [PermissionCategory]
 public static class PropSpawner
 {
+    [StaffOnly]
     public const string All = "vMenu.Enhanced.Menus.PropSpawner.All";
 
+    [StaffOnly]
     public const string Menu = "vMenu.Enhanced.Menus.PropSpawner.Menu";
 
+    [StaffOnly]
     public const string Spawn = "vMenu.Enhanced.Menus.PropSpawner.Spawn";
 
+    [StaffOnly]
     public const string Networked = "vMenu.Enhanced.Menus.PropSpawner.Networked";
 
+    [StaffOnly]
     public const string Sets = "vMenu.Enhanced.Menus.PropSpawner.Sets";
 
+    [StaffOnly]
     public const string SetsManage = "vMenu.Enhanced.Menus.PropSpawner.SetsManage";
 
+    [StaffOnly]
     public const string Delete = "vMenu.Enhanced.Menus.PropSpawner.Delete";
 
     [StaffOnly]
